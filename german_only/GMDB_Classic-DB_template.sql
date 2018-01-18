5e6253195dfbf6f4dcada6c15bac50b69b0d6ce7
Saugjunkie <Saugjunkie@web.de>
2018-01-18 21:47:26 +0100

    dbscripts die ersten 100 eintraege hinzugefugt

---
+++ german_only/GMDB_Classic-DB_template.sql
@@ -0,0 +1,82 @@
+SET NAMES 'utf8';
+
+UPDATE `dbscript_string` SET `content_default`='Danke, $N Und viel Glück!' WHERE (`entry`='2000000000'); /* content: Thank you, $N. And luck to you. */
+UPDATE `dbscript_string` SET `content_default`='Die Klippenquellfälle befinden sich in den Bergen Richtung Osten und Norden.' WHERE (`entry`='2000000001'); /* Cliffspring Falls lies along the mountains to the east and north. */
+UPDATE `dbscript_string` SET `content_default`='Geht rasch, $N Mein Schicksal liegt in Eurer Hand.' WHERE (`entry`='2000000002'); /* Go swiftly $N. My fate is in your hands. */
+UPDATE `dbscript_string` SET `content_default`='Großmutter Falkenwind deutet auf die Wasserkanne, die auf dem Rand des Brunnens steht.' WHERE (`entry`='2000000003'); /* %s gestures to the pitcher of water sitting on the edge of the well. */
+UPDATE `dbscript_string` SET `content_default`='Es ist lange her, Bethor, mein Freund.' WHERE (`entry`='2000000004'); /* It has been a long time, Bethor, my friend. */
+UPDATE `dbscript_string` SET `content_default`='Wenn es die Zeit erlaubt, dann müssen wir uns in Ruhe unterhalten, denn wir haben viel zu besprechen.' WHERE (`entry`='2000000005'); /* When time permits, we must speak at length. For we have much to discuss. */
+UPDATE `dbscript_string` SET `content_default`='Und danke, $N Ohne Eure Hilfe hätte ich den Weg zu den Verlassenen nie gefunden.' WHERE (`entry`='2000000006'); /* And thank you, $N.  Without your aid I may never have found my way to the Forsaken. */
+UPDATE `dbscript_string` SET `content_default`='Lebt wohl, mein Freund.' WHERE (`entry`='2000000007'); /* Farewell, my friend. */
+UPDATE `dbscript_string` SET `content_default`='%s schnuppert an dem Elixier und langt dann eifrig zu!' WHERE (`entry`='2000000008'); /* %s sniffs the elixir then eagerly digs in! */
+UPDATE `dbscript_string` SET `content_default`='Ah, das ist genau richtig! Ich glaube, ich hole mir noch ein paar... könnt Ihr für mich auf diese Fässer aufpassen, $N' WHERE (`entry`='2000000012'); /* Ah, that sure does hit the spot! I think i\'ll get myself a couple more...can you watch these barrels for me, $N? */
+UPDATE `dbscript_string` SET `content_default`='Und wie läuft das Fassbewachen...?' WHERE (`entry`='2000000013'); /* How goes the barrel watching...? */
+UPDATE `dbscript_string` SET `content_default`='Nun ja, ich muss mich wieder um die Arbeit kümmern. Aber so eine kleine Pause tut schon gut und so ein Donnerbräu Lager schmeckt immer!' WHERE (`entry`='2000000014'); /* Well, back to business for me. But it sure was nice taking that short break, and it\'s always nice drinking Thunder Ale! */
+UPDATE `dbscript_string` SET `content_default`='Ich danke Euch, $n. Und mein Bruder dankt Euch ebenfalls.' WHERE (`entry`='2000000016'); /* I thank you. $n. And my brother thanks you. */
+UPDATE `dbscript_string` SET `content_default`='%s beginnt zu singen, während er die Quellsteine und das Bernsteinkorn vor dem Stammesfeuer vermischt.' WHERE (`entry`='2000000017'); /* %s begins chanting as he mixes the well stones and ambercom before the Tribal Fire. */
+UPDATE `dbscript_string` SET `content_default`='Das Wasser der Seher steht für Euch zum Trinken bereit, $N' WHERE (`entry`='2000000018'); /* The Water of the Seers is ready for your consumption, $N. */
+UPDATE `dbscript_string` SET `content_default`='Habt Geduld, $N. Die Fackel ist fast fertig.' WHERE (`entry`='2000000019'); /* Be patient $N. The torch is almost complete. */
+UPDATE `dbscript_string` SET `content_default`='Das ist interessant...' WHERE (`entry`='2000000020'); /* That\'s interesting... */
+UPDATE `dbscript_string` SET `content_default`='Ich muss Noram und Horatio kontaktieren.' WHERE (`entry`='2000000021'); /* I need to consult Noram and Horatio. */
+UPDATE `dbscript_string` SET `content_default`='Geht jetzt, $N. Findet Volcor... und denkt daran, dass die Magie, die Euch beschützt, versagen wird, wenn Ihr mit anderen mehr tut als bloß zu reden.' WHERE (`entry`='2000000022'); /* Go now, $N. Find Volcor... and remember, the magics protecting you will fail if you interact with anyone, save for talking. */
+UPDATE `dbscript_string` SET `content_default`='%s beginnt, einen Zauber zu wirken, als Ihr ihm den Umhang abnehmt.' WHERE (`entry`='2000000023'); /* %s begins to cast a spell as you take the cloak from him. */
+UPDATE `dbscript_string` SET `content_default`='M... m... mir... geht... es... nicht... gut...' WHERE (`entry`='2000000024'); /* %s I. . .I. . .don\'t. . .feel. . .right. . . */
+UPDATE `dbscript_string` SET `content_default`='Mein Verstand... mein Fleisch... ich... verwese...!' WHERE (`entry`='2000000025'); /* My mind. . .my flesh. . .I\'m. . .rotting. . . .! */
+UPDATE `dbscript_string` SET `content_default`='Ups!' WHERE (`entry`='2000000026'); /* Whoops! */
+UPDATE `dbscript_string` SET `content_default`='Das Volk von Westfall salutiert vor $N, einem mutigen und tapferen Verteidiger der Freiheit.' WHERE (`entry`='2000000028'); /* The People of Westfall salute $N, a brave and valiant defender of freedom. */
+UPDATE `dbscript_string` SET `content_default`='Das macht Sinn!' WHERE (`entry`='2000000030'); /* This makes sense! */
+UPDATE `dbscript_string` SET `content_default`='Vorsicht, $n! Schaut nach Westen!' WHERE (`entry`='2000000031'); /* Beware, $n! Look to the west! */
+UPDATE `dbscript_string` SET `content_default`='Seid gewappnet, $N, denn der Dämon in dieser Kugel ist eine Teufelsbestie.' WHERE (`entry`='2000000032'); /* Gird yourself $N, for the demon in this orb is a fel beast. */
+UPDATE `dbscript_string` SET `content_default`='%s beschwört einen Boten...' WHERE (`entry`='2000000033'); /* %s summons a messenger... */
+UPDATE `dbscript_string` SET `content_default`='Lass mich dir dabei helfen.' WHERE (`entry`='2000000034'); /* Let me help you with those. */
+UPDATE `dbscript_string` SET `content_default`='Trink das, es wird dir helfen' WHERE (`entry`='2000000035'); /* Drink this, it will help. */
+UPDATE `dbscript_string` SET `content_default`='Dir wird es gut gehen' WHERE (`entry`='2000000036'); /* You\'re going to be just fine. */
+UPDATE `dbscript_string` SET `content_default`='Bis wir uns wiedersehen, tapfere Seele.' WHERE (`entry`='2000000042'); /* Until we meet again, brave one. */
+UPDATE `dbscript_string` SET `content_default`='Es ist vollbracht...' WHERE (`entry`='2000000044'); /* It is done... */
+UPDATE `dbscript_string` SET `content_default`='Es geht doch nichts über kochend heißes Morgengebräu an einem kalten Morgen in Dun Morogh! Danach kann der Tag kommen!' WHERE (`entry`='2000000045'); /* There\'s nothing like some scalding mornbrew on a chilly Dun Morogh day to get things started right! */
+UPDATE `dbscript_string` SET `content_default`='Ooooh! Heiß! Heiß! Heiß! Wenn Euch das nicht auf Touren bringt, kann ich Euch auch nicht helfen!' WHERE (`entry`='2000000046'); /* Oooooo hot hot hot! If that won\'t put spring IN your step,I don\'t know what will! */
+UPDATE `dbscript_string` SET `content_default`='Verflixt! Nein!!! Das kann... verflixt... nicht sein! Ihr habt mich... verflixt... mit Euren faulen Tricks betrogen! Verflixt!' WHERE (`entry`='2000000047'); /* Ribbit! No!! This cannot... ribbit... be! You have duped me with... ribbit... your foul trickery! Ribbit! */
+UPDATE `dbscript_string` SET `content_default`='Lasst uns nun diese seltene Erde in meinen Pflanzbehälter füllen...' WHERE (`entry`='2000000048'); /* Now let us place this rare earth in my planter... */
+UPDATE `dbscript_string` SET `content_default`='Ich danke Euch nochmals, $N. Diese seltene Erde wird mir bei meinen Experimenten von großem Nutzen sein.' WHERE (`entry`='2000000049'); /* Thank you again, $N. This rare earth will be very helpful in my experiments. */
+UPDATE `dbscript_string` SET `content_default`='Hey, Belm! Gib mir einen Becher Donnerbräu Lager und einen für meinen guten Freund $N.' WHERE (`entry`='2000000056'); /* Hey there, Belm! Give me a mug of Thunder Ale, and one for my good friend $N. */
+UPDATE `dbscript_string` SET `content_default`='Geister des Wassers, ich preise euch und erflehe einen Gefallen von euch. Ihr habt die Bitten gehört, die $N an euch gerichtet hat, und ich bin mir sicher, ihre Beweggründe sind lauter und edel. Bitte, helft uns.' WHERE (`entry`='2000000057'); /* Spirits of Water, I give you praise, and I beg of you a favour. You have heard $N\'s please, and I trust his inent is noble. Please, will you aid us? */
+UPDATE `dbscript_string` SET `content_default`='Dank Euch, großer Geist. Danke!' WHERE (`entry`='2000000058'); /* Thank you great spirit. Thank you! */
+UPDATE `dbscript_string` SET `content_default`='Ihr seid entlassen, $n.' WHERE (`entry`='2000000059'); /* You are dismissed, $N. */
+UPDATE `dbscript_string` SET `content_default`='Folgt mir, guter Paladin. Ich werde Euer Pferdegeschirr schneller fertigen, als ich ein Zwergen - Starkbier hinunterspülen kann. Mmmm.... Starkbier.' WHERE (`entry`='2000000063'); /* Follow me, good $C. I\'ll have your barding done faster than I could down a Dwarven stout. Mmmm... stout. */
+UPDATE `dbscript_string` SET `content_default`='Nun, das sollte ausreichen. Komm jetzt, lasst uns zurück zum Laden gehen, um das Geschäft zu beenden!' WHERE (`entry`='2000000064'); /* Well, that should do it. Come now, back to the shop to finish our business! */
+UPDATE `dbscript_string` SET `content_default`='Wir werden keinen Dämonen-Diener in unseren Ländern dulden!' WHERE (`entry`='2000000065'); /* We will suffer no demon\'s servant in our lands! */
+UPDATE `dbscript_string` SET `content_default`='Friede und Geduld sei mit Euch, $N. Bleibt immer stark.' WHERE (`entry`='2000000066'); /* Peace and patience be with you, $N. Remain strong always. */
+UPDATE `dbscript_string` SET `content_default`='%s versucht, Maurys Klumpfuß zu öffnen...' WHERE (`entry`='2000000067'); /* %s tries opening Maury\'s Clubbed Foot... */
+UPDATE `dbscript_string` SET `content_default`='Pah! $N, dieser Fuß gibt nicht nach!' WHERE (`entry`='2000000068'); /* Bah! $N, this foot won\'t budge! */
+UPDATE `dbscript_string` SET `content_default`='Danke, $N.' WHERE (`entry`='2000000069'); /* Thank you, $N. */
+UPDATE `dbscript_string` SET `content_default`='Ich bin sicher, dass ich eine Verwendung für diese Mixtur finden werde...' WHERE (`entry`='2000000070'); /* I\'m sure I can find a use for this mixture... */
+UPDATE `dbscript_string` SET `content_default`='Nochmals vielen Dank für Ihre Hilfe. Möge der Wind während Ihrer Reisen immer in Ihrem Rücken sein' WHERE (`entry`='2000000071'); /* Thank you again for your aid. May the wind always be at your back during your travels. */
+UPDATE `dbscript_string` SET `content_default`='Das war einfacher als ich dachte... das Siegelzeichen von Thoradin ist endlich wieder ganz.' WHERE (`entry`='2000000072'); /* Easier than I expected... the sigil of Thoradin is whole, once again. */
+UPDATE `dbscript_string` SET `content_default`='Wartet, $N!!! Es sieht so aus, als würden wir hier Hilfe brauchen...' WHERE (`entry`='2000000073'); /* Wait, $N!!! It looks like we\'re going to need some help here... */
+UPDATE `dbscript_string` SET `content_default`='In den Kasten, meine Süßen! Genau so. Einer nach dem anderen.' WHERE (`entry`='2000000074'); /* Into the box me pretties! Thas it. One by one, ye go. */
+UPDATE `dbscript_string` SET `content_default`='Hört her, Bewohner von Tanaris! Ihr sollt wissen, dass $n ein erhabenes Mitglied des Mithrilordens ist. Ein Schmied von Ehre, Hingabe und unendlicher Geduld. Ein dreifaches Hoch auf $n!' WHERE (`entry`='2000000076'); /* Hear ye, denizens of Tanaris! Let it be known $n is an exalted member of the Mithril Order. A blacksmith of honor, dedication, and infinite patience. Three cheers for $n */
+UPDATE `dbscript_string` SET `content_default`='Hört her, Bewohner von Tanaris! Ihr sollt wissen, dass $n ein erhabenes Mitglied des Mithrilordens ist. Ein Schmied von Ehre, Hingabe und unendlicher Geduld. Ein dreifaches Hoch auf $n%s spürt, wie sein Blut in Wallung gerät und die Arglist des Raptors ihn durchströmt.' WHERE (`entry`='2000000077'); /* %s watches as his blood begins to stir, and the guile of the raptor slips through him. */
+UPDATE `dbscript_string` SET `content_default`='Was ist das für eine Stärke, die mich durchfließt? Ist dies die Macht des Schamanismus, von der Torgan gesprochen hat...?' WHERE (`entry`='2000000078'); /* What is this power that flows through me? Is this the power of shamanism that Tor\'gan spoke of...? */
+UPDATE `dbscript_string` SET `content_default`='Beeindruckend! Ich fühle mich... erneuert. Meine Kraft kehrt zurück!' WHERE (`entry`='2000000079'); /* Amazing! I feel... renewed. My strength returns! */
+UPDATE `dbscript_string` SET `content_default`='%s beginnt einen Ritus der Erschaffung...' WHERE (`entry`='2000000080'); /* %s begins a rite of creation... */
+UPDATE `dbscript_string` SET `content_default`='Ich habe das Totem erschaffen. Ihr, $N, müsst den Brunnen läutern.' WHERE (`entry`='2000000081'); /* I have created the totem. You, $N, must cleanse the well. */
+UPDATE `dbscript_string` SET `content_default`='%s erschafft ein neues Totem...' WHERE (`entry`='2000000082'); /* %s creates a new totem... */
+UPDATE `dbscript_string` SET `content_default`='Das Totem ist fertig, $N.' WHERE (`entry`='2000000083'); /* The totem is made, $N. */
+UPDATE `dbscript_string` SET `content_default`='%s verwandelt die Gegenstände von $n in ein Totem der Läuterung...' WHERE (`entry`='2000000084'); /* %s transforms the items $N gave him into a totem of cleansing... */
+UPDATE `dbscript_string` SET `content_default`='Das Totem ist fertig. Der Brunnen kann nun geläutert werden.' WHERE (`entry`='2000000085'); /* The totem is made. The well may now be cleansed. */
+UPDATE `dbscript_string` SET `content_default`='%s betrachtet das Pergament aus dem Folianten der Schattenhammerkultisten...' WHERE (`entry`='2000000086'); /* %s studies the parchment from the Twilight Tome... */
+UPDATE `dbscript_string` SET `content_default`='%s wird von freigesetzter Magie getroffen!' WHERE (`entry`='2000000087'); /* %s is struck by unleashed magic! */
+UPDATE `dbscript_string` SET `content_default`='Hmmm...' WHERE (`entry`='2000000088'); /* Hmm... */
+UPDATE `dbscript_string` SET `content_default`='Die Geister des Steinkrallengebirges wüten noch immer, $n.' WHERE (`entry`='2000000089'); /* The spirits of Stonetalon still rage, $N. */
+UPDATE `dbscript_string` SET `content_default`='Ich fürchte, wir werden sie niemals beschwichtigen können...' WHERE (`entry`='2000000090'); /* I fear we may never soothe them... */
+UPDATE `dbscript_string` SET `content_default`='%s hört sich die Bitte von $n an.' WHERE (`entry`='2000000091'); /* %s listens to $N\'s plea. */
+UPDATE `dbscript_string` SET `content_default`='Ihr habt gut daran getan zu kommen. Meine Weisheit gehört Euch.' WHERE (`entry`='2000000092'); /* You were right to come. My wisdom will be yours. */
+UPDATE `dbscript_string` SET `content_default`='Geht, mein Diener. Bringt Kunde vom Steinkrallengebirge zu Apothekerin Zamah.' WHERE (`entry`='2000000093'); /* Go, my servant. Send word of Stonetalon to Apothecary Zamah. */
+UPDATE `dbscript_string` SET `content_default`='%s übergibt Apothekerin Zamah eine Nachricht.' WHERE (`entry`='2000000094'); /* %s gives message to Apothecary Zamah. */
+UPDATE `dbscript_string` SET `content_default`='%s lächelt.' WHERE (`entry`='2000000095'); /* %s smiles. */
+UPDATE `dbscript_string` SET `content_default`='%s erschafft sein Toxin...' WHERE (`entry`='2000000096'); /* %s creates his toxin... */
+UPDATE `dbscript_string` SET `content_default`='Da wären wir, $n. Das Toxin steht bereit.' WHERE (`entry`='2000000097'); /* There we are, $N. The toxin is ready. */
+UPDATE `dbscript_string` SET `content_default`='%s füllt einen Behälter mit dem Toxin.' WHERE (`entry`='2000000098'); /* %s fills a container with the toxin. */
+UPDATE `dbscript_string` SET `content_default`='Das Toxin ist fertig, $n.' WHERE (`entry`='2000000099'); /* %s The toxin is ready, $N. */
+UPDATE `dbscript_string` SET `content_default`='%s stellt die Kiste mit den Teilen auf den Boden.' WHERE (`entry`='2000000100'); /* %s places the crate of parts on the ground. */
+UPDATE `dbscript_string` SET `content_default`='Hmmm... ich glaube ich brauche einen besseren Köder. Ich werde mal im Wagen meines Vaters nachsehen.' WHERE (`entry`='2000000201'); /* Hmmm... I think I need better bait. I\'ll check my father\'s wagon. */
